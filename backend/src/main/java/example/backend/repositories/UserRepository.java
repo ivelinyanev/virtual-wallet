472c4a58442da67f897d83105ca5b72891f24798
ivelinyanev <123307555+ivelinyanev@users.noreply.github.com>
2025-12-28 17:26:15 +0200

    Add main repositories

---
+++ backend/src/main/java/example/backend/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package example.backend.repositories;
+
+import example.backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
