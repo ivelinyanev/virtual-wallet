e5933d64e501adff79ccf612f2cfd3e9cb49c2b4
ivelinyanev <123307555+ivelinyanev@users.noreply.github.com>
2025-12-30 00:39:34 +0200

    Add Transaction and Wallet repositories

---
+++ backend/src/main/java/example/backend/repositories/WalletRepository.java
@@ -0,0 +1,9 @@
+package example.backend.repositories;
+
+import example.backend.models.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+}
