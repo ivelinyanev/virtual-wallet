76bd76a1fe5333deafb1cd618971a8b0b8b336f0
ivelinyanev <123307555+ivelinyanev@users.noreply.github.com>
2026-01-06 14:02:22 +0200

    Add MockBean of MailSender to fix build issue

--- backend/src/test/java/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/example/backend/BackendApplicationTests.java
@@ -2,11 +2,16 @@ package example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class BackendApplicationTests {
+
+    @MockBean
+    private JavaMailSender mailSender;
 
     @Test
     void contextLoads() {
