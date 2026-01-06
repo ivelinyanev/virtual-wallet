6134b0be22ac3e057ab9b14b0abd19e8eaf873c6
ivelinyanev <123307555+ivelinyanev@users.noreply.github.com>
2026-01-06 13:31:06 +0200

    Retry fix on EmailConfig for @Value that fail in CI

--- backend/src/main/java/example/backend/config/EmailConfig.java
+++ backend/src/main/java/example/backend/config/EmailConfig.java
@@ -9,6 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
+@Profile("!test")
 @Configuration
 public class EmailConfig {
 
@@ -18,7 +19,6 @@ public class EmailConfig {
     @Value("${email.password}")
     private String emailPassword;
 
-    @Profile("!test")
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
